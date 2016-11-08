7d4432378d965536c0cea02befb0cb6fbf012525
Aaron <avergara08@gmail.com>
2016-11-08 10:19:23 +0800

    Progress updates on New Customer form

--- sites/all/modules/custom/avscustomers/templates/forms/avscustomers-add-form.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php
-print '<div class="uk-panel uk-panel-box">' . render($variables['form']['title']) . '</div>';
-hide($variables['form']['first_name']);
-print drupal_render_children($variables['form']);
-?>
-x
--- sites/all/themes/uikit_avs/template.php
+++ sites/all/themes/uikit_avs/template.php
@@ -69,3 +69,37 @@ function uikit_avs_preprocess_block(&$variables) {
  */
 function uikit_avs_process_block(&$variables) {
 }
+
+/**
+ * Overrides theme_textfield().
+ * @param $variables
+ * @return string
+ */
+function uikit_avs_textfield($variables) {
+  $element = $variables['element'];
+  $element['#attributes']['type'] = 'text';
+  element_set_attributes($element, array('id', 'name', 'value', 'size', 'maxlength'));
+  $classes = array('form-text', 'uk-width-1-1');
+  if (isset($element['#parents']) && form_get_error($element)) {
+    $classes[] = 'uk-form-danger';
+  }
+  _form_set_class($element, $classes);
+
+  $extra = '';
+  if ($element['#autocomplete_path'] && !empty($element['#autocomplete_input'])) {
+    drupal_add_library('system', 'drupal.autocomplete');
+    $element['#attributes']['class'][] = 'form-autocomplete';
+
+    $attributes = array();
+    $attributes['type'] = 'hidden';
+    $attributes['id'] = $element['#autocomplete_input']['#id'];
+    $attributes['value'] = $element['#autocomplete_input']['#url_value'];
+    $attributes['disabled'] = 'disabled';
+    $attributes['class'][] = 'autocomplete';
+    $extra = '<input' . drupal_attributes($attributes) . ' />';
+  }
+
+  $output = '<input' . drupal_attributes($element['#attributes']) . ' />';
+
+  return $output . $extra;
+}
