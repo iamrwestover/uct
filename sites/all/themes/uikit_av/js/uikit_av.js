a974af9c3badb3e467645181c11313eae58d22e3
James <avmeta08@yahoo.com>
2016-11-30 17:16:25 +0800

    Started maintenance modules for category groups

--- sites/all/themes/uikit_av/js/uikit_av.js
+++ sites/all/themes/uikit_av/js/uikit_av.js
@@ -3,8 +3,7 @@
 }(jQuery));
 
 jQuery(document).ready(function ($) {
-  // Here we will call the function with jQuery as the parameter once the page
-  // Document Object Model (DOM) is ready for JavaScript code to execute.
+  // Toggle offcanvas menu.
   var $panel = $('#menu-toggle');
   $panel.find('.uk-offcanvas-bar').hover(function () {
     if ($panel.hasClass('offcanvas-expanded')) {
@@ -13,7 +12,6 @@ jQuery(document).ready(function ($) {
     UIkit.offcanvas.show('#menu-toggle', {mode: 'reveal'});
     $panel.removeClass('offcanvas-collapsed');
     $panel.addClass('offcanvas-expanded');
-    console.log('expanded');
   }, function () {
     if ($panel.hasClass('offcanvas-collapsed')) {
       return;
@@ -21,7 +19,6 @@ jQuery(document).ready(function ($) {
     UIkit.offcanvas.hide();
     $panel.removeClass('offcanvas-expanded');
     $panel.addClass('offcanvas-collapsed');
-    console.log('collapsed');
   });
 });
 
