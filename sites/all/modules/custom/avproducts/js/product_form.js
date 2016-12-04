6b82c34a69c52e6a337bd9d29903e10d8ec6e1c9
James <avmeta08@yahoo.com>
2016-12-04 19:00:00 +0800

    Progress updates UOM adding on product add/edit page

--- sites/all/modules/custom/avproducts/js/product_form.js
+++ sites/all/modules/custom/avproducts/js/product_form.js
@@ -3,56 +3,26 @@
  */
 
 (function ($) {
-  // Here we immediately call the function with jQuery as the parameter.
+  /**
+   * General behaviors on Product Add/Edit form.
+   * @type {{attach: Function}}
+   */
+  Drupal.behaviors.avproductsEditForm = {
+    attach: function (context, settings) {
+      $('.uom-group-select:not(.processed)').change(function() {
+        var uomIndex = $(this).data('uom-index');
+        var $qtyElement = $('#edit-uom-group-uoms-' + uomIndex + '-qty');
+        $qtyElement.attr('title', 'x');
+      }).addClass('processed');
+    }
+  };
 }(jQuery));
 
 jQuery(document).ready(function ($) {
-  $('.repeater-uom').repeater({
-    // (Optional)
-    // start with an empty list of repeaters. Set your first (and only)
-    // "data-repeater-item" with style="display:none;" and pass the
-    // following configuration flag
-    initEmpty: true,
-    // (Optional)
-    // "defaultValues" sets the values of added items.  The keys of
-    // defaultValues refer to the value of the input's name attribute.
-    // If a default value is not specified for an input, then it will
-    // have its value cleared.
-    defaultValues: {
-      'text-input': 'foo'
-    },
-    // (Optional)
-    // "show" is called just after an item is added.  The item is hidden
-    // at this point.  If a show callback is not given the item will
-    // have $(this).show() called on it.
-    show: function () {
-      $(this).slideDown('fast');
-    },
-    // (Optional)
-    // "hide" is called when a user clicks on a data-repeater-delete
-    // element.  The item is still visible.  "hide" is passed a function
-    // as its first argument which will properly remove the item.
-    // "hide" allows for a confirmation step, to send a delete request
-    // to the server, etc.  If a hide callback is not given the item
-    // will be deleted.
-    hide: function (deleteElement) {
-      if(confirm('Are you sure you want to delete this element?')) {
-        $(this).slideUp(deleteElement);
-      }
-    },
-    // (Optional)
-    // You can use this if you need to manually re-index the list
-    // for example if you are using a drag and drop library to reorder
-    // list items.
-    ready: function (setIndexes) {
-      //$dragAndDrop.on('drop', setIndexes);
-    },
-    // (Optional)
-    // Removes the delete button from the first list item,
-    // defaults to false.
-    isFirstItemUndeletable: true
-  });
+
 });
+
+
 
 jQuery(document).load(function ($) {
   // Here we will call the function with jQuery as the parameter once the entire
