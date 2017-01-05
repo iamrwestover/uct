77721d926f865412eb42f4714b564e3d663a9cba
Aaron <avergara08@gmail.com>
2017-01-05 19:03:22 +0800

    Progress updates + addition of mail modules

--- sites/all/themes/uikit_av/templates/forms/customers/avcustomers-customer-form.tpl.php
+++ sites/all/themes/uikit_av/templates/forms/customers/avcustomers-customer-form.tpl.php
@@ -43,27 +43,24 @@ $form['address']['address']['#attributes']['placeholder'] = 'Building / Unit # /
       <div class="uk-width-2-6 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['middle_name']); ?></div>
       <div class="uk-width-2-6 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['last_name']); ?></div>
 
-      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['code']); ?></div>
+      <!--<div class="uk-width-1-3 --><?php //print $vertical_margin_class; ?><!--">--><?php //print drupal_render($form['info']['code']); ?><!--</div>-->
       <div class="uk-width-2-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['display_name']); ?></div>
+      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['_created']); ?></div>
 
       <div class="uk-width-2-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['agent_id']); ?></div>
-      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['_created']); ?></div>
     </div>
   </div>
 
   <div class="uk-width-1-2">
     <div class="uk-grid uk-grid-small">
-      <div class="uk-width-1-2 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['contact']['email']); ?></div>
-      <div class="uk-width-1-2 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['contact']['contact_number']); ?></div>
+      <div class="uk-width-1-1 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['contact']['email']); ?></div>
+      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['contact']['phone1']); ?></div>
+      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['contact']['phone2']); ?></div>
+      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['contact']['phone3']); ?></div>
 
-      <div class="uk-width-1-1 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['contact']['website']); ?></div>
+      <!--<div class="uk-width-1-1 --><?php //print $vertical_margin_class; ?><!--">--><?php //print drupal_render($form['contact']['website']); ?><!--</div>-->
 
-      <div class="uk-width-1-2 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['credit_limit']); ?></div>
-      <div class="uk-width-1-2 <?php print $vertical_margin_class; ?>"></div>
 
-      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['term_id']); ?></div>
-      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['discount_type']); ?></div>
-      <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['discount_value']); ?></div>
     </div>
   </div>
 
@@ -71,7 +68,6 @@ $form['address']['address']['#attributes']['placeholder'] = 'Building / Unit # /
     <ul class="uk-tab" data-uk-tab="{connect:'#more-info'}">
       <li><a href="">Address</a></li>
       <li><a href="">Payment</a></li>
-      <li><a href="">Miscellaneous</a></li>
     </ul>
     <ul id="more-info" class="uk-switcher">
       <li class="uk-panel uk-panel-box">
@@ -92,8 +88,25 @@ $form['address']['address']['#attributes']['placeholder'] = 'Building / Unit # /
         </div>
 
       </li>
-      <li class="uk-panel uk-panel-box"><?php print drupal_render($form['payment']['payment_method_id']); ?></li>
-      <li class="uk-panel uk-panel-box"><?php print drupal_render($form['misc']['notes']); ?></li>
+      <li class="uk-panel uk-panel-box">
+        <div class="uk-grid">
+          <div class="uk-width-1-2">
+            <div class="uk-grid uk-grid-small">
+              <div class="uk-width-1-1 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['payment']['payment_method_id']); ?></div>
+              <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['term_id']); ?></div>
+              <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['discount_type']); ?></div>
+              <div class="uk-width-1-3 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['info']['discount_value']); ?></div>
+            </div>
+          </div>
+
+          <div class="uk-width-1-2">
+            <div class="uk-grid uk-grid-small">
+              <div class="uk-width-1-2 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['payment']['opening_balance']); ?></div>
+              <div class="uk-width-1-2 <?php print $vertical_margin_class; ?>"><?php print drupal_render($form['payment']['credit_limit']); ?></div>
+            </div>
+          </div>
+        </div>
+      </li>
     </ul>
   </div>
 </div>
