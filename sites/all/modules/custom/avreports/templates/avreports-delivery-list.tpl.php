8d3287e6cdc9eab3bcb9e05ef15a8ada9af52b2e
Aaron <avergara08@gmail.com>
2017-01-24 05:09:41 +0800

    Added advagg module for future use. Finished delivery list report.

---
+++ sites/all/modules/custom/avreports/templates/avreports-delivery-list.tpl.php
@@ -0,0 +1,13 @@
+<div class="uk-grid uk-margin-top">
+  <div class="uk-width-1-2">
+    <?php print drupal_render($form['agent']); ?>
+  </div>
+  <div class="uk-width-1-2 uk-text-right">
+    <?php print drupal_render($form['transaction_date']); ?>
+  </div>
+
+</div>
+<?php print drupal_render($form['table']); ?>
+<?php
+//print drupal_render
+//dpm($form);
