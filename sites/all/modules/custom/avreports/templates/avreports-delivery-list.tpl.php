3c9c55b3483e3322d4cc85c416f2bd46f37d2ff0
Aaron <avergara08@gmail.com>
2017-01-24 06:13:32 +0800

    Minor fixes to Delivery List report

--- sites/all/modules/custom/avreports/templates/avreports-delivery-list.tpl.php
+++ sites/all/modules/custom/avreports/templates/avreports-delivery-list.tpl.php
@@ -5,9 +5,18 @@
   <div class="uk-width-1-2 uk-text-right">
     <?php print drupal_render($form['transaction_date']); ?>
   </div>
-
 </div>
+
 <?php print drupal_render($form['table']); ?>
+
+<div class="uk-grid uk-margin-top">
+  <div class="uk-width-1-2">
+    <?php print drupal_render($form['prepared_by']); ?>
+  </div>
+  <div class="uk-width-1-2 uk-text-right">
+    <?php print drupal_render($form['delivered_by']); ?>
+  </div>
+</div>
 <?php
 //print drupal_render
 //dpm($form);
