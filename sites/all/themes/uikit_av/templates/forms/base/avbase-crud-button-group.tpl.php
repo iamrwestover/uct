cd8c65c34de51c6fc7b42c083b6cd2349b149506
Aaron <avergara08@gmail.com>
2017-02-20 23:08:04 +0800

    Add new row button now adds 20 rows instead of 1

--- sites/all/themes/uikit_av/templates/forms/base/avbase-crud-button-group.tpl.php
+++ sites/all/themes/uikit_av/templates/forms/base/avbase-crud-button-group.tpl.php
@@ -6,7 +6,7 @@
     $buttons[$position][] = $form[$key];
   }
 ?>
-<div class="uk-grid uk-margin-top">
+<div class="uk-grid uk-margin-top not-printable">
   <div class="uk-width-1-2"><?php print drupal_render($buttons['opposite']); ?></div>
   <div class="uk-width-1-2 uk-text-right"><?php print drupal_render($buttons['normal']); ?></div>
 </div>
