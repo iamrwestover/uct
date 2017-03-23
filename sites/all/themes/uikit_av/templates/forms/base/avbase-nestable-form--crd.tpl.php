9346cce1ab12d7466684725e963656c7c4f295cd
Aaron <avergara08@gmail.com>
2017-03-23 23:16:01 +0800

    Credit memo transactions

---
+++ sites/all/themes/uikit_av/templates/forms/base/avbase-nestable-form--crd.tpl.php
@@ -0,0 +1,41 @@
+<?php
+$view_mode = !empty($form['#av_view_mode']);
+?>
+<div class="av-nestable-form uk-nestable av-nestable-product-list-form<?php print ($view_mode ? ' av-nestable-product-list-view' : ''); ?>">
+  <div class="av-nestable-form-header">
+    <div class="uk-grid uk-grid-collapse uk-text-bold uk-text-uppercase">
+      <div class="uk-width-4-10">
+        <div class="uk-grid uk-grid-collapse">
+          <div class="uk-width-1-2 uk-text-center" style="width: 7%;"><div class="av-nestable-cell">#</div></div>
+          <div class="uk-width-1-2" style="width: 93%;"><div class="av-nestable-cell">Account</div></div>
+        </div>
+      </div>
+
+      <div class="uk-width-6-10">
+        <div class="uk-grid uk-grid-collapse">
+          <div class="uk-width-<?php print ($view_mode ? '9' : '8'); ?>-10">
+            <div class="av-nestable-cell">
+             Description
+            </div>
+          </div>
+          <div class="uk-width-1-10">
+            <div class="av-nestable-cell uk-text-<?php print ($view_mode ? 'right' : 'center'); ?>">
+              Amount
+            </div>
+          </div>
+          <?php if (empty($view_mode)): ?>
+            <div class="uk-width-1-10">
+              <div class="av-nestable-cell">
+                <!--Actions-->
+              </div>
+            </div>
+          <?php endif; ?>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <?php print drupal_render_children($form); ?>
+  <div id="item-list-new-product-wrapper"></div>
+</div>
---
+++ sites/all/themes/uikit_av/templates/forms/base/avbase-nestable-form-row--crd.tpl.php
@@ -0,0 +1,69 @@
+<?php
+$view_mode = !empty($form['#av_view_mode']);
+//foreach (element_children($form) as $key) {
+//  $form[$key]['#title_display'] = 'none';
+//}
+
+//$form['product_title']['#attributes']['placeholder'] = 'product code or name';
+
+//$form['uom_title']['#attributes']['class'][] = 'uk-text-center';
+//$form['qty']['#attributes']['class'][] = 'uk-text-right';
+//$form['cost']['#attributes']['class'][] = 'uk-text-right';
+//$form['discount']['#attributes']['class'][] = 'uk-text-right';
+//$form['total']['#attributes']['class'][] = 'uk-text-right';
+
+// Style delete btn.
+//$form['prod_delete_btn']['#button_label'] = '<i class="uk-icon-remove"></i>';
+//$form['prod_delete_btn']['#attributes']['class'][] = 'av-ajax-trigger';
+//$form['prod_delete_btn']['#attributes']['class'][] = 'av-ajax-trigger-icon-only';
+//$form['prod_delete_btn']['#attributes']['class'][] = 'uk-button-danger';
+//$form['prod_delete_btn']['#attributes']['class'][] = 'uk-button-mini';
+//$form['prod_delete_btn']['#attributes']['class'][] = 'uk-text-muted';
+
+?>
+
+<div class="uk-grid uk-grid-collapse" style="position: relative;">
+  <?php print drupal_render($form['badge']); ?>
+  <div class="uk-width-4-10">
+
+    <div class="uk-grid uk-grid-collapse">
+      <div class="uk-width-1-2 uk-text-center" style="width: 7%;">
+        <div class="av-nestable-cell">
+          <span class="av-nestable-row-num" style="<?php print $view_mode ? 'top: 0;' : ''; ?>"><?php print $form['#prod_index'] + 1; ?></span>
+        </div>
+      </div>
+      <div class="uk-width-1-2" style="width: 93%;">
+        <div class="av-nestable-cell">
+          <?php print drupal_render($form['account_name']); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="uk-width-6-10">
+
+    <div class="uk-grid uk-grid-collapse">
+
+      <div class="uk-width-<?php print ($view_mode ? '9' : '8'); ?>-10">
+        <div class="av-nestable-cell">
+          <?php print drupal_render($form['description']); ?>
+        </div>
+      </div>
+
+      <div class="uk-width-1-10">
+        <div class="av-nestable-cell uk-text-right">
+          <?php print drupal_render($form['total']); ?>
+        </div>
+      </div>
+
+      <?php if (empty($view_mode)): ?>
+        <div class="uk-width-1-10 uk-text-center">
+          <div class="av-nestable-cell">
+            <?php print drupal_render($form['prod_delete_btn']); ?>
+          </div>
+        </div>
+      <?php endif; ?>
+    </div>
+
+  </div>
+</div>
