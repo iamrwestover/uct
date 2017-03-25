48f0d8c4587e9a8a895a944341f3c663ee490d9a
Aaron <avergara08@gmail.com>
2017-03-25 23:39:05 +0800

    Minor breadcrumb changes

--- sites/all/themes/uikit_av/theme/breadcrumb.tpl.php
+++ sites/all/themes/uikit_av/theme/breadcrumb.tpl.php
@@ -15,6 +15,13 @@
 $breadcrumb = $variables['breadcrumb'];
 $path = explode('/', current_path());
 
+// Alter breadcrumb on backup and migrate admin pages.
+if (arg(3) == 'backup_migrate' && $user->uid != 1) {
+  unset($breadcrumb[1]);
+  unset($breadcrumb[2]);
+  unset($breadcrumb[3]);
+}
+
 // Provide a navigational heading to give context for breadcrumb links to
 // screen-reader users. Make the heading invisible with .uk-hidden.
 $output = '<h2 class="uk-hidden">' . t('You are here') . '</h2>';
@@ -25,4 +32,5 @@ foreach ($breadcrumb as $crumb) {
 $page_title = drupal_is_front_page() ? t('Home') : drupal_get_title();
 $output .= '<li>' . $page_title . '</li>';
 $output .= '</ul>';
+
 print $output;
