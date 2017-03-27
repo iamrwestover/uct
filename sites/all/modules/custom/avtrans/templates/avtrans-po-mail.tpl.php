3c0b0e3cf7043268222921055f8c3af71e3dd6c5
Aaron <avergara08@gmail.com>
2017-03-28 05:36:06 +0800

    Removed unnecessary files and directories. Added admin ability to modify file system settings

--- sites/all/modules/custom/avtrans/templates/avtrans-po-mail.tpl.php
+++
@@ -1,9 +0,0 @@
-Dear <?php print $vendor_name; ?>,
-
-Please find our purchase order details below.
-
-<?php print check_plain($po->message); ?>
-<?php print $po_table; ?>
-
-Thanks,
-<?php print $site_name; ?>
