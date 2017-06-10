bcd7974181954b41e4f73d1e83940523d1d1b3d0
Aaron <avergara08@gmail.com>
2017-06-10 13:30:04 +0800

    Added System requirements information on help page

---
+++ sites/all/modules/custom/avbase/templates/help/avbase-help-sys-req.tpl.php
@@ -0,0 +1,21 @@
+<?php
+//var_dump(get_defined_vars());
+?>
+<h2 id="system-requirements"><?php print $help_title; ?></h2>
+<h6 class="uk-text-uppercase">Recommended Server Specifications</h6>
+<ul>
+  <li>Intel Core i7, 8GB RAM</li>
+  <li><span class="uk-text-bold">64-bit</span> Windows 7 or higher</li>
+  <li>Latest 64-bit version of Google Chrome</li>
+</ul>
+<h6 class="uk-text-uppercase">Minimum Server Specifications</h6>
+<ul>
+  <li>Intel Core i3, 4GB RAM</li>
+  <li><span class="uk-text-bold">64-bit</span> Windows 7 or higher</li>
+  <li>Latest 64-bit version of Google Chrome</li>
+</ul>
+<h6 class="uk-text-uppercase">Recommended Client Computer Specifications</h6>
+<ul>
+  <li><span class="uk-text-bold">64-bit</span> Windows 7 or higher</li>
+  <li>Latest 64-bit version of Google Chrome</li>
+</ul>
